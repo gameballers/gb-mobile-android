59565dc5586777a774762a16e210fa3981545879
mbadr <mbadr@deemalab.com>
2019-02-16 17:32:36 +0200

    call achievements api and mapping data done

--- gameballsdk/src/main/java/com/gameball/gameball/views/achievements/AchievementsFragment.java
+++ gameballsdk/src/main/java/com/gameball/gameball/views/achievements/AchievementsFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.gameball.gameball.R;
 import com.gameball.gameball.model.response.Game;
@@ -15,16 +16,20 @@ import com.gameball.gameball.views.profile.AchievementsAdapter;
 
 import java.util.ArrayList;
 
-public class AchievementsFragment extends Fragment {
+public class AchievementsFragment extends Fragment implements AchievemetsContract.View
+{
     View rootView;
     private RecyclerView achievementsRecyclerView;
+    private ProgressBar loadingIndicator;
 
     AchievementsAdapter achievementsAdapter;
+    AchievemetsContract.Presenter presenter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         achievementsAdapter = new AchievementsAdapter(getContext(), new ArrayList<Game>());
+        presenter = new AchievementsPresenter(getContext(), this);
     }
 
     @Nullable
@@ -32,13 +37,34 @@ public class AchievementsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_achievements, container, false);
         initView();
+        presenter.getAchievements();
         return rootView;
     }
 
     private void initView() {
         achievementsRecyclerView = rootView.findViewById(R.id.achievements_recyclerView);
+        loadingIndicator = rootView.findViewById(R.id.loading_indicator);
         achievementsRecyclerView.setHasFixedSize(true);
         achievementsRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
         achievementsRecyclerView.setAdapter(achievementsAdapter);
+    }
+
+    @Override
+    public void fillAchievements(ArrayList<Game> games)
+    {
+        achievementsAdapter.setmData(games);
+        achievementsAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void showLoadingIndicator()
+    {
+        loadingIndicator.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideLoadingIndicator()
+    {
+        loadingIndicator.setVisibility(View.GONE);
     }
 }
